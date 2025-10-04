93896b4a18362ab4fd2209296d8be78f7a5143d0
Nabil Kashyap <nkashyap@getty.edu>
2025-10-04 13:30:31 -0700

    add logging to fetch handler

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("ヽ༼ຈل͜ຈ༽ﾉ / hurrah! / ヽ༼ຈل͜ຈ༽ﾉ", { status: 200 });
+    const msg = "ヽ༼ຈل͜ຈ༽ﾉ / hurrah! / ヽ༼ຈل͜ຈ༽ﾉ";
+    console.log(msg);
+    return new Response(msg, { status: 200 });
   },
 };
