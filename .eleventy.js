92eb64bb9cca656327125041e61b10cc38f7a772
Nabil Kashyap <nkashyap@getty.edu>
2026-01-24 13:38:28 -0800

    add target blank to md parsing + update hover styles

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,22 @@ const mdOptions = {
 const md = new MarkdownIt(mdOptions);
 md.use(MarkdownItAttrs);
 
+/** @h/t https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#rules **/
+// Remember the old renderer if overridden, or proxy to the default renderer.
+const defaultRender =
+  md.renderer.rules.link_open ||
+  function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  // Add a new `target` attribute, or replace the value of the existing one.
+  tokens[idx].attrSet("target", "_blank");
+
+  // Pass the token to the default renderer.
+  return defaultRender(tokens, idx, options, env, self);
+};
+
 export default (eleventyConfig) => {
   /**
    * Render settings for markdown parser
